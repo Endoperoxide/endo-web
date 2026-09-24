abf0a619bccc13b961873d5d0f1e3698d041e88a
Endoperoxide <benreevesmoore8@gmail.com>
2026-09-24 22:28:17 +0100

    Updates

--- assets/GameTitle-Veh5npVL.js
+++ assets/GameTitle-Veh5npVL.js
@@ -1,4 +1,4 @@
-import{s as e}from"./PageDivider-BRnvrZvy.js";var t=`---\r
+import{s as e}from"./PageDivider-DEbazbx9.js";var t=`---\r
 slug: "a-hat-in-time"\r
 title: "A Hat in Time"\r
 year: 2017\r
@@ -356,18 +356,43 @@ year: 2026\r
 platforms:\r
   - PC\r
   - PlayStation\r
+  - Switch\r
 coverUrl: "https://images.igdb.com/igdb/image/upload/t_cover_big_2x/co7h9m.jpg"\r
-reviewDate:\r
-playtimeHours:\r
-rating:\r
+reviewDate: "2026-09-24"\r
+playtimeHours: 35\r
+rating: 9.0\r
 categories:\r
-  gameplay:\r
-  story:\r
-  music:\r
-  soundDesign:\r
-  visualDesign:\r
-  replayability:\r
+  gameplay: 9.2\r
+  story: 0.0\r
+  music: 8.5\r
+  soundDesign: 9.0\r
+  visualDesign: 8.5\r
+  replayability: 6.5\r
 ---\r
+\r
+## Context\r
+\r
+Big Walk is primarily an open world, Co-op, puzzle game, intended to be played exclusively with 2+ players. The intended design of this game is to have friends collaborate with one another to solve small puzzles across a very large map, of which is used to end the game.\r
+\r
+This game is arguably the most special social experience you can achieve through a video game. Every single step is beautifully put together in a way that meaningfully interacts with your friends, whilst providing a beautiful visual and auditory experience.\r
+\r
+The concept of trying to work together with a group of friends to understand the puzzle you are about to complete together, then assigning who wants to do what, and then performing that strategy is so insanely entertaining to me. It also helps that the cute and silly art-style that this game has really complements how you play the game and mess with your friends. Due to the sheer size of the map, even the "Walk" aspect of the game becomes enjoyable, where the puzzles allow you to unlock quicker (and more scenic) ways to explore the map, which goes hand-in-hand with the way the map is structured. The open world part of this game really shines as there is no linear path to take, if you and your friends decide to go to a puzzle opposite side of the map, you are more than welcome to do so. And making up exploration ideas on the spot with a map in hand gives a hiking-like feel to the game which I personally have never seen a game do before.\r
+\r
+It does an excellent job of increasing the difficulty and intricacy of the puzzles the more you understand the mechanics and exploration of it. Of which, the puzzles in this game are so uniquely interesting, that of the 45 total puzzles within the game, almost none of them feel alike, which makes approaching a newly found area so much more exciting. Additionally, the game does a fantastic job of having people engage with one another using limitations and encouraging saving specific gear types to communicate with each other better.\r
+\r
+I think what makes this game so special is what it takes away. For example, your character is unable to equip or un-equip items by themselves, and requires someone else to do it for you. This includes the colors of your character too, which serves as a great form of engaging with your friends to work together. Additionally, because of this mechanic, it makes it really fun to mess with your friends, which is always bonus points.\r
+\r
+The game also never specifies the names of items, which you and your friends design personal names for each items. For example the red oblong objects we called them "eggs", but i've heard that other groups have called them alternative names such as "keys", "dolls", "plumbuses" etc. And i just think this adds to the feeling of this game so well.\r
+\r
+In the puzzles the game also likes to remove some form of communication off of you, which requires different strategies of how to address the solving thought-process. Things such as voice, visibility, sight, etc are all things that Big Walk tends to remove from you, which makes the puzzles that more interesting, and the teamwork dynamic that much more engaging.\r
+\r
+## Final Challenge\r
+\r
+The ending of this game is phenomenal, a challenge to prove of how well you have gotten to work with your friends by limiting your most important asset, your voice. Working through this gauntlet of quite difficult puzzles together without any audio, and simply using your body as a tool for communication is done so well. Additionally, this final gauntlet removes your custom made colors you have given each other, which also makes it quite fun to differentiate who is who from merely your friends' play-style alone.\r
+\r
+## Summary\r
+\r
+To summarize, Big Walk is a fantastically crafted puzzle game that really encourages engaging with your friends to solve excellently crafted puzzles with each other. The environment, sounds and art-style are so well put together that makes the entire experience phenomenal. Which is amplified ten fold when you play it with a group of friends you get along so well with. Oddly enough, this game feels nostalgic to look back on, despite having played it so recently.\r
 `,f=`---\r
 slug: "bin-weevils"\r
 title: "Bin Weevils"\r
